c5060185d3f9db7958354cf6e306cb43d465a975
Rikerd <cursedmarkedsasuke@gmail.com>
2019-09-27 13:44:27 -0700

    Pathlib
    Update code to use pathlib in order to rename
    Organize code into functions

--- fileRenamer.py
+++ fileRenamer.py
@@ -1,95 +1,107 @@
+from pathlib import Path
 import os
 
-def baseHyphenIndex():
+# Find directory
+def findDirectory()->Path:
+    '''Returns directory'''
+    pathInput = input("Enter folder directory: ")
+    path = Path(pathInput)
+
+    if path.exists() == False or not path.is_dir():
+        print("Directory not found!")
+        findDirectory()
+        
+    print("Directory Found!\n")
+    
+    return path
+
+# Rename files to have base name, hyphen, index
+def baseHyphenIndex(p: Path):
+    '''Name - 01'''
     currentVideoIndex = 1;
     
-    for file in os.listdir():
-        fileName, fileExtension = os.path.splitext(file)
-    
+    for file in p.iterdir():
         if currentVideoIndex <= 9:
             hyphenString = " - 0"
         else:
             hyphenString = " - "
     
-        newFileName = videoNameBase + hyphenString + str(currentVideoIndex) + fileExtension
+        newFileName = videoNameBase + hyphenString + str(currentVideoIndex) + file.suffix
     
-        os.rename(file, newFileName)
+        file.rename(p.joinpath(newFileName))
 
         currentVideoIndex += 1
-
-def baseIndex():
+        
+# Rename files to have base name, index
+def baseIndex(p: Path):
+    '''Name 01'''
     currentVideoIndex = 1;
 
-    for file in os.listdir():
-        fileName, fileExtension = os.path.splitext(file)
-    
+    for file in p.iterdir():
         if currentVideoIndex <= 9:
             spaceString = " 0"
         else:
             spaceString = " "
     
-        newFileName = videoNameBase + spaceString + str(currentVideoIndex) + fileExtension
+        newFileName = videoNameBase + spaceString + str(currentVideoIndex) + file.suffix
     
-        os.rename(file, newFileName)
+        file.rename(p.joinpath(newFileName))
 
         currentVideoIndex += 1
 
-def indexHyphenBase():
+# Rename files to have index, hyphen, base name
+def indexHyphenBase(p: Path):
+    '''01 - Name'''
     currentVideoIndex = 1;
 
-    for file in os.listdir():
-        fileName, fileExtension = os.path.splitext(file)
-    
+    for file in p.iterdir():    
         if currentVideoIndex <= 9:
             startString = "0"
         else:
             startString = ""
     
-        newFileName = startString + str(currentVideoIndex) + " - " + videoNameBase + fileExtension
+        newFileName = startString + str(currentVideoIndex) + " - " + videoNameBase + file.suffix
     
-        os.rename(file, newFileName)
+        file.rename(p.joinpath(newFileName))
 
         currentVideoIndex += 1
 
-def indexBase():
+# Rename files to have index, base name
+def indexBase(p: Path):
+    '''01 Name'''
     currentVideoIndex = 1;
 
-    for file in os.listdir():
-        fileName, fileExtension = os.path.splitext(file)
-    
+    for file in p.iterdir():    
         if currentVideoIndex <= 9:
             startString = "0"
         else:
             startString = ""
     
-        newFileName = startString + str(currentVideoIndex) + " " + videoNameBase + fileExtension
+        newFileName = startString + str(currentVideoIndex) + " " + videoNameBase + file.suffix
     
-        os.rename(file, newFileName)
+        file.rename(p.joinpath(newFileName))
 
         currentVideoIndex += 1
 
 
-dirPath = input("Enter folder directory: ")
+if __name__ == '__main__':
+    path = findDirectory()
+    
+    print("CHOOSE NAMING CONVENTION")
+    print("(1) Name Base - 01")
+    print("(2) Name Base 01")
+    print("(3) 01 - Name Base")
+    print("(4) 01 Name Base")
 
-os.chdir(dirPath)
+    conventionChoice = input("\nEnter convention choice: ")
 
-print("Directory Found!\n")
+    videoNameBase = input("Enter file name base: ")
 
-print("CHOOSE NAMING CONVENTION")
-print("(1) Name Base - 01")
-print("(2) Name Base 01")
-print("(3) 01 - Name Base")
-print("(4) 01 Name Base")
-
-conventionChoice = input("\nEnter convention choice: ")
-
-videoNameBase = input("Enter video name base: ")
-
-if conventionChoice == "1":
-    baseHyphenIndex()
-elif conventionChoice == "2":
-    baseIndex()
-elif conventionChoice == "3":
-    indexHyphenBase()
-elif conventionChoice == "4":
-    indexBase()
+    if conventionChoice == "1":
+        baseHyphenIndex(path)
+    elif conventionChoice == "2":
+        baseIndex(path)
+    elif conventionChoice == "3":
+        indexHyphenBase(path)
+    elif conventionChoice == "4":
+        indexBase(path)
